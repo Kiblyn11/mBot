fb6e7c48fadc05169710997baf7cb78b9a7029b6
Kiblyn11 <4669951+Kiblyn11@users.noreply.github.com>
2022-12-10 19:11:21 +0100

    Update targets.go

--- targets/targets.go
+++ targets/targets.go
@@ -34,13 +34,10 @@ func CheckTargets(target string) {
 
 		if len(bodyString) > 3 { // Check to see if there is a JSON response
 			for i := 0; i < len(newTargets); i++ { // Iterate over the number of unregistered targets
-				if newTargets[i].Category.ID >= 3 { // Check to see if new target is a web/host target
-				} else {
-					if !newTargets[i].Registered {
-						log.Printf(env.InfoColor, "Onboarding to "+newTargets[i].Codename+" - "+newTargets[i].Slug)
-						OnboardTarget("https://platform.synack.com/api/targets/" + newTargets[i].Slug + "/signup")
-						discord.ConnectDiscord("Onboarded to " + newTargets[i].Codename + "! Go find some vulns!!")
-					}
+				if !newTargets[i].Registered {
+					log.Printf(env.InfoColor, "Onboarding to "+newTargets[i].Codename+" - "+newTargets[i].Slug)
+					OnboardTarget("https://platform.synack.com/api/targets/" + newTargets[i].Slug + "/signup")
+					discord.ConnectDiscord("Onboarded to " + newTargets[i].Codename + "! Go find some vulns!!")
 				}
 			}
 		}
@@ -72,7 +69,7 @@ func CheckForQR(target string) {
 
 		if len(bodyString) > 3 { // Check to see if there is a JSON response
 			for i := 0; i < len(newTargets); i++ { // Iterate over the number of unregistered targets
-				if newTargets[i].Category.ID < 3 && !checkIfNotified(notified, newTargets[i].Codename) { // Check to see if new target is a web/host target
+				if !checkIfNotified(notified, newTargets[i].Codename) { // Check to see if new target is a web/host target
 					log.Printf(env.SuccessColor, "[!] Target "+newTargets[i].Codename+" is in QR!")
 					discord.ConnectDiscord("Target " + newTargets[i].Codename + " is in QR! Go find some vulns!!")
 					notified = append(notified, newTargets[i].Codename)
